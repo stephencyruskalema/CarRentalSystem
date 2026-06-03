ffd6aa1ff856392c0cfae6f2e2f857cd7260e7f0
Stephen Cyrus Kalema <stephencyruskalema@gmail.com>
2026-06-03 16:22:37 +0300

    config: optimize settings for pythonanywhere migration

--- core/settings.py
+++ core/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-local-key-for-dev')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fallback-key-for-coursework-123')
 
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == ['True', '1']
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.1', 'localhost', '.onrender.com', '.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
